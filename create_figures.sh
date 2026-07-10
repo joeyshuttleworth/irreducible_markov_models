4dbf0fbdd3adc2f20fe7bcccfb9be218d310f5f8
Joseph G. Shuttleworth <joey.shuttleworth@notitngham.ac.uk>
2026-07-10 17:46:37 +0100

    Use temp venv in create_figures.sh

--- create_figures.sh
+++ create_figures.sh
@@ -2,7 +2,10 @@
 
 mkdir -p paper_figures
 
-. .venv/bin/activate
+python3 -m venv .tmp_venv
+. .tmp_venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python3 scripts/degenerate_model_demonstration.py -o paper_output
 python3 scripts/compare_model_reductions.py -o paper_output
@@ -13,3 +16,6 @@ cp paper_output/simple_drug_model/drug_model_output.pdf paper_figures/Fig5.pdf
 cp paper_output/compare_model_reductions/compare_reductions.pdf paper_figures/Fig6.pdf
 cp paper_output/degenerate_model_demo/degenerate_model_fig.pdf paper_figures/Fig7.pdf
 cp paper_output/bifurcation_plot/bifurcation_diagram.pdf paper_figures/Fig8.pdf
+
+deactivate
+rm -rf .tmp_venv
